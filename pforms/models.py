097c5fba229f4b898d75e7cb6265f9e8f5f61575
polialessandro <89081706+polialessandro@users.noreply.github.com>
2021-08-17 14:57:03 +0200

    Update models.py

--- pforms/models.py
+++ pforms/models.py
@@ -1,12 +1,27 @@
-from pforms import db
-from dataclasses import dataclass
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
 
-@dataclass
-class User(db.Model):
-    id: int
-    username: str
-    email: str
+engine = create_engine('sqlite://',echo=True)
+Base=declerative_base()
+
+class Utenti(Base):
+    __tablename__='Utenti'			
+    id_utente: Column(Integer, primarykey=True)
+
+class Domande(Base):
+    __tablename__='Domande'
+    id_domanda: Column(Integer, primarykey=True)
+    testo: Column(String)
     
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+class Risposte(Base):
+    __tablename__='Risposte'
+    id_risposta: Column(Integer, primarykey=True)
+    testo: Column(String)
+    
+
+class Categorie(Base):
+    __tablename__='Categorie'
+    id_categoria: Column(Integer, primarykey=True)
+    tipo: Column(String)
